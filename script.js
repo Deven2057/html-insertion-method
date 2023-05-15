8f17f373f90a7625b5b9b6559ed265388c6bbc0d
Devendra Bhattarai <91841677+Deven2057@users.noreply.github.com>
2023-05-15 16:41:35 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+let a= document.getElementsByTagName("div")[0]
+a.innerHTML=a.innerHTML+"<h1>hello bro is it nice</h1>"
+
+let div= document.createElement('div');
+div.innerHTML="<h1>hello bro </h1>";
+a.append(div);
+a.prepend(div);
+a.before(div);
+a.after(div);
+a.replaceWith(div);
